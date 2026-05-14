0c52b42e55d532f358963726d650dda6a18cf3e1
ikimodus <ifhumail@gmail.com>
2026-05-14 18:03:37 +0200

    JS file + addition working

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-if ((true, false, true, false, true, false, true, false, true, false)) {
+let clickValue = 0;
+const clickerBtn = document.querySelector(".click-area");
+const clickerCounter = document.querySelector(".click-counter");
+
+function onBtnClicked() {
+  clickValue += 1;
+  clickerCounter.textContent = clickValue;
 }
 
-console.log("hey");
+clickerBtn.onclick = onBtnClicked;
